cfcbccfa5d5cafaa6d810737ccae459686229b89
TCOD <tcoddev@gmail.com>
2024-06-29 20:13:38 +0330

    update style and responsive design

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const translateText = () => {
   const input_text = document.getElementById("input_text").value;
   const output_text = document.getElementById("output_text");
+  const select_target_lang =
+    document.getElementById("select_target_lang").value;
+  const select_lang = document.getElementById("select_lang").value;
 
-  googleTranslateApi(input_text, "en", "fil").then(
+  if (input_text.trim() === "") {
+    Toastify({
+      text: "لطفا فیلد را پر کنید",
+
+      duration: 3000,
+    }).showToast();
+  }
+
+  googleTranslateApi(input_text, select_lang, select_target_lang).then(
     (res) => (output_text.value = res.text)
   );
+};
+
+const clearFeild = () => {
+  input_text.value = "";
+  output_text.value = "";
 };
 
 // Google Translate API

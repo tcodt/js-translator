86c96f57a8b052b2f123832a12dc200175a35b78
TCOD <tcoddev@gmail.com>
2024-06-29 16:28:10 +0330

    add project logic

---
+++ app.js
@@ -0,0 +1,21 @@
+const translateText = () => {
+  const input_text = document.getElementById("input_text").value;
+  const output_text = document.getElementById("output_text");
+
+  googleTranslateApi(input_text, "en", "fa").then(
+    (res) => (output_text.value = res.text)
+  );
+};
+
+// Google Translate API
+const googleTranslateApi = (text, sourceLang, targetLang) => {
+  return fetch(
+    `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURI(
+      text
+    )}`
+  )
+    .then((response) => response.json())
+    .then((data) => ({
+      text: data[0][0][0],
+    }));
+};
